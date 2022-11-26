489640298fea1295bce8c6b4ddc1c8ac2f1acbd6
yana0616 <rainymoment0616@gmail.com>
2022-11-26 22:08:41 +0900

    [add / fix]flexの共通スタイルをheaderで読み込む（css modules）

--- components/header.js
+++ components/header.js
@@ -1,12 +1,15 @@
 import Logo from "./logo";
 import Nav from "./nav";
+import styles from "styles/header.module.sass"
 
 
 export default function Header() {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   )
 }
